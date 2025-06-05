98031ac78b63966dc0aa9b6866b39babc27f326a
shy9803 <127914332+shy9803@users.noreply.github.com>
2025-06-05 12:39:52 +0900

    Update Goods.js

--- src/component/Goods.js
+++ src/component/Goods.js
@@ -17,7 +17,7 @@ function Goods(props) {
     //React비동기 통신
     axios
     //DB에서 json데이터를 불러온다.
-    .get('https://port-0-backend-mbiobig1cd0dc4c0.sel4.cloudtype.app/goods')
+    .get('backend/goods')
     //성공시 데이터를 변수에 저장
     .then(res=>{
       setData(res.data);
